1f96bf84a7cce4068ef7cd517163689faaa4a9b6
Nelson-Menro <Nelson.mendez7@gmail.com>
2021-09-26 21:53:16 -0400

    added in logic for carousel

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,1 +1,51 @@
+
+
 let slidePosition = 0;
+const slides = document.querySelectorAll(".carousel_item");
+const totalSlides = slides.length;
+
+//This launches every time we press next button
+document.querySelector('#carousel_button--next').addEventListener("click", function(){
+    nextSlide();
+
+});
+
+document.querySelector('#carousel_button--previous').addEventListener("click", function(){
+    previousSlide();
+});
+
+function updateSlidePosition(){
+    //loop through image array
+    for(let slide of slides){
+        slide.classList.remove('carousel_item--visible');
+        slide.classList.add('carousel_item--hidden');
+    }
+
+    slides[slidePosition].classList.add('carousel_item--visible');
+}
+
+function nextSlide(){
+    //if at last slide, then go back to starting
+    
+    if(slidePosition == totalSlides -1){
+        slidePosition = 0;
+    }else{
+        slidePosition++;
+    }
+
+    updateSlidePosition();
+}
+
+
+function previousSlide(){
+    
+    if(slidePosition == 0){
+        slidePosition = totalSlides-1;
+    }else{
+        slidePosition--;
+    }
+    updateSlidePosition();
+}
+
+
+
