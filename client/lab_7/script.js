3b73df1677175172445b0da360b4cc4a131731a3
Nelson-Menro <Nelson.mendez7@gmail.com>
2021-10-14 12:21:16 -0400

    added in submit button and event listener

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -3,6 +3,7 @@
 async function windowsAction() {
   const endPoint = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
   const cities = await endPoint.json();
+  const submitButton = document.querySelector('#submitButton');
 
   // eslint-disable-next-line no-shadow
   function searchMatches(wordToMatch, cities) {
@@ -12,11 +13,33 @@ async function windowsAction() {
     });
   }
 
+  function placeMarkers() {
+    console.log('test!');
+  }
+
+  submitButton.addEventListener('click', (evt) => {
+    placeMarkers();
+  });
+
+  function mapInit() {
+    mymap = L.map('mapid').setView([38.989, -76.93], 12);
+    const ACCESSTOKEN = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+    L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${ACCESSTOKEN}`, {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'your.mapbox.access.token'
+    }).addTo(mymap);
+  }
+
+  mapInit();
+
   const suggestions = document.querySelector('.suggestions');
 
   function displayMatches(event) {
     const matchedArray = searchMatches(event.target.value, cities);
-    // eslint-disable-next-line no-template-curly-in-string
     // eslint-disable-next-line arrow-body-style
     const html = matchedArray.map((place) => {
       return `<li><span>${place.name}<br>${place.category}<br><i>${place.address_line_1}</i><br><i>${place.city}</i><br><i>${place.zip}</i><br></span></li><br>`;
@@ -30,17 +53,6 @@ async function windowsAction() {
   searchInput.addEventListener('keyup', (evt) => {
     displayMatches(evt);
   });
-
-  const mymap = L.map('mapid').setView([38.989, -76.93], 12);
-  const ACCESSTOKEN = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
-  L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${ACCESSTOKEN}`, {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-  }).addTo(mymap);
 }
 
 window.onload = windowsAction();
